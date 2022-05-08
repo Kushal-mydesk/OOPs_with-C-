cb3f7279c4212b24a67e19da234fa5ed0d887585
Kushal Mondal <kushal99mondal@gmail.com>
2022-05-08 14:25:09 +0530

    first commit

---
+++ cmake-build-debug/CMakeFiles/C___Program_From_The_Book_.dir/DependInfo.cmake
@@ -0,0 +1,50 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "E:/Documents/C++ Program(From The Book)/Extras/intpart_fracpart.cpp" "E:/Documents/C++ Program(From The Book)/cmake-build-debug/CMakeFiles/C___Program_From_The_Book_.dir/Extras/intpart_fracpart.cpp.obj"
+  "E:/Documents/C++ Program(From The Book)/Extras/practice1.cpp" "E:/Documents/C++ Program(From The Book)/cmake-build-debug/CMakeFiles/C___Program_From_The_Book_.dir/Extras/practice1.cpp.obj"
+  "E:/Documents/C++ Program(From The Book)/Extras/theGetchLibrary.cpp" "E:/Documents/C++ Program(From The Book)/cmake-build-debug/CMakeFiles/C___Program_From_The_Book_.dir/Extras/theGetchLibrary.cpp.obj"
+  "E:/Documents/C++ Program(From The Book)/Functions/Global_variables.cpp" "E:/Documents/C++ Program(From The Book)/cmake-build-debug/CMakeFiles/C___Program_From_The_Book_.dir/Functions/Global_variables.cpp.obj"
+  "E:/Documents/C++ Program(From The Book)/Functions/default_arguements.cpp" "E:/Documents/C++ Program(From The Book)/cmake-build-debug/CMakeFiles/C___Program_From_The_Book_.dir/Functions/default_arguements.cpp.obj"
+  "E:/Documents/C++ Program(From The Book)/Functions/engdisp.cpp" "E:/Documents/C++ Program(From The Book)/cmake-build-debug/CMakeFiles/C___Program_From_The_Book_.dir/Functions/engdisp.cpp.obj"
+  "E:/Documents/C++ Program(From The Book)/Functions/overload.cpp" "E:/Documents/C++ Program(From The Book)/cmake-build-debug/CMakeFiles/C___Program_From_The_Book_.dir/Functions/overload.cpp.obj"
+  "E:/Documents/C++ Program(From The Book)/Functions/recursion.cpp" "E:/Documents/C++ Program(From The Book)/cmake-build-debug/CMakeFiles/C___Program_From_The_Book_.dir/Functions/recursion.cpp.obj"
+  "E:/Documents/C++ Program(From The Book)/Functions/reference_arguements.cpp" "E:/Documents/C++ Program(From The Book)/cmake-build-debug/CMakeFiles/C___Program_From_The_Book_.dir/Functions/reference_arguements.cpp.obj"
+  "E:/Documents/C++ Program(From The Book)/Functions/vararg.cpp" "E:/Documents/C++ Program(From The Book)/cmake-build-debug/CMakeFiles/C___Program_From_The_Book_.dir/Functions/vararg.cpp.obj"
+  "E:/Documents/C++ Program(From The Book)/Objects & Classes/cards.cpp" "E:/Documents/C++ Program(From The Book)/cmake-build-debug/CMakeFiles/C___Program_From_The_Book_.dir/Objects_&_Classes/cards.cpp.obj"
+  "E:/Documents/C++ Program(From The Book)/Objects & Classes/counter.cpp" "E:/Documents/C++ Program(From The Book)/cmake-build-debug/CMakeFiles/C___Program_From_The_Book_.dir/Objects_&_Classes/counter.cpp.obj"
+  "E:/Documents/C++ Program(From The Book)/Objects & Classes/ecopycon.cpp" "E:/Documents/C++ Program(From The Book)/cmake-build-debug/CMakeFiles/C___Program_From_The_Book_.dir/Objects_&_Classes/ecopycon.cpp.obj"
+  "E:/Documents/C++ Program(From The Book)/Objects & Classes/englcon.cpp" "E:/Documents/C++ Program(From The Book)/cmake-build-debug/CMakeFiles/C___Program_From_The_Book_.dir/Objects_&_Classes/englcon.cpp.obj"
+  "E:/Documents/C++ Program(From The Book)/Objects & Classes/englobj.cpp" "E:/Documents/C++ Program(From The Book)/cmake-build-debug/CMakeFiles/C___Program_From_The_Book_.dir/Objects_&_Classes/englobj.cpp.obj"
+  "E:/Documents/C++ Program(From The Book)/Objects & Classes/englret.cpp" "E:/Documents/C++ Program(From The Book)/cmake-build-debug/CMakeFiles/C___Program_From_The_Book_.dir/Objects_&_Classes/englret.cpp.obj"
+  "E:/Documents/C++ Program(From The Book)/Objects & Classes/objpart.cpp" "E:/Documents/C++ Program(From The Book)/cmake-build-debug/CMakeFiles/C___Program_From_The_Book_.dir/Objects_&_Classes/objpart.cpp.obj"
+  "E:/Documents/C++ Program(From The Book)/Objects & Classes/smallobj.cpp" "E:/Documents/C++ Program(From The Book)/cmake-build-debug/CMakeFiles/C___Program_From_The_Book_.dir/Objects_&_Classes/smallobj.cpp.obj"
+  "E:/Documents/C++ Program(From The Book)/Structures/cards.cpp" "E:/Documents/C++ Program(From The Book)/cmake-build-debug/CMakeFiles/C___Program_From_The_Book_.dir/Structures/cards.cpp.obj"
+  "E:/Documents/C++ Program(From The Book)/Structures/checkstrc.cpp" "E:/Documents/C++ Program(From The Book)/cmake-build-debug/CMakeFiles/C___Program_From_The_Book_.dir/Structures/checkstrc.cpp.obj"
+  "E:/Documents/C++ Program(From The Book)/Structures/englarea.cpp" "E:/Documents/C++ Program(From The Book)/cmake-build-debug/CMakeFiles/C___Program_From_The_Book_.dir/Structures/englarea.cpp.obj"
+  "E:/Documents/C++ Program(From The Book)/Structures/englstrc.cpp" "E:/Documents/C++ Program(From The Book)/cmake-build-debug/CMakeFiles/C___Program_From_The_Book_.dir/Structures/englstrc.cpp.obj"
+  "E:/Documents/C++ Program(From The Book)/Structures/parts.cpp" "E:/Documents/C++ Program(From The Book)/cmake-build-debug/CMakeFiles/C___Program_From_The_Book_.dir/Structures/parts.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/C___Program_From_The_Book_.dir/cmake_clean.cmake
@@ -0,0 +1,34 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C___Program_From_The_Book_.dir/Extras/intpart_fracpart.cpp.obj"
+  "CMakeFiles/C___Program_From_The_Book_.dir/Extras/practice1.cpp.obj"
+  "CMakeFiles/C___Program_From_The_Book_.dir/Extras/theGetchLibrary.cpp.obj"
+  "CMakeFiles/C___Program_From_The_Book_.dir/Functions/Global_variables.cpp.obj"
+  "CMakeFiles/C___Program_From_The_Book_.dir/Functions/default_arguements.cpp.obj"
+  "CMakeFiles/C___Program_From_The_Book_.dir/Functions/engdisp.cpp.obj"
+  "CMakeFiles/C___Program_From_The_Book_.dir/Functions/overload.cpp.obj"
+  "CMakeFiles/C___Program_From_The_Book_.dir/Functions/recursion.cpp.obj"
+  "CMakeFiles/C___Program_From_The_Book_.dir/Functions/reference_arguements.cpp.obj"
+  "CMakeFiles/C___Program_From_The_Book_.dir/Functions/vararg.cpp.obj"
+  "CMakeFiles/C___Program_From_The_Book_.dir/Objects_&_Classes/cards.cpp.obj"
+  "CMakeFiles/C___Program_From_The_Book_.dir/Objects_&_Classes/counter.cpp.obj"
+  "CMakeFiles/C___Program_From_The_Book_.dir/Objects_&_Classes/ecopycon.cpp.obj"
+  "CMakeFiles/C___Program_From_The_Book_.dir/Objects_&_Classes/englcon.cpp.obj"
+  "CMakeFiles/C___Program_From_The_Book_.dir/Objects_&_Classes/englobj.cpp.obj"
+  "CMakeFiles/C___Program_From_The_Book_.dir/Objects_&_Classes/englret.cpp.obj"
+  "CMakeFiles/C___Program_From_The_Book_.dir/Objects_&_Classes/objpart.cpp.obj"
+  "CMakeFiles/C___Program_From_The_Book_.dir/Objects_&_Classes/smallobj.cpp.obj"
+  "CMakeFiles/C___Program_From_The_Book_.dir/Structures/cards.cpp.obj"
+  "CMakeFiles/C___Program_From_The_Book_.dir/Structures/checkstrc.cpp.obj"
+  "CMakeFiles/C___Program_From_The_Book_.dir/Structures/englarea.cpp.obj"
+  "CMakeFiles/C___Program_From_The_Book_.dir/Structures/englstrc.cpp.obj"
+  "CMakeFiles/C___Program_From_The_Book_.dir/Structures/parts.cpp.obj"
+  "C___Program_From_The_Book_.exe"
+  "C___Program_From_The_Book_.exe.manifest"
+  "C___Program_From_The_Book_.pdb"
+  "libC___Program_From_The_Book_.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C___Program_From_The_Book_.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
